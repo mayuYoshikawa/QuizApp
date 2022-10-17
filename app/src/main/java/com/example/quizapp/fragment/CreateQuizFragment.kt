8ab07dc48e4e336c4570b94cb176896bc6b671c4
mayu yoshikawa <m.yoshikawa25@gmail.com>
2022-10-17 17:29:36 +0900

    画面遷移とxml修正2

---
+++ app/src/main/java/com/example/quizapp/fragment/CreateQuizFragment.kt
@@ -0,0 +1,39 @@
+package com.example.quizapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentCreateQuizBinding
+
+class CreateQuizFragment : Fragment() {
+
+    private var _binding: FragmentCreateQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //val view = inflater.inflate(R.layout.fragment_top, container, false)
+
+        _binding = FragmentCreateQuizBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createQuizButton.setOnClickListener {
+            findNavController().navigate(R.id.action_topFragment_to_selectFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/fragment/TopFragment.kt
@@ -0,0 +1,43 @@
+package com.example.quizapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentTopBinding
+
+class TopFragment : Fragment() {
+
+    private var _binding: FragmentTopBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //クイズボタン
+        binding.topQuizButton.setOnClickListener {
+            findNavController().navigate(R.id.action_topFragment_to_selectFragment)
+        }
+        //問題作成ボタン
+        binding.topCreateQuizButton.setOnClickListener {
+            findNavController().navigate(R.id.action_topFragment_to_createQuiz)
+        }
+
+    }
+}
